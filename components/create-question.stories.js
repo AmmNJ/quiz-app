f222f33575c2f2b8bd925a5444c16d769c520e87
Ammersken Nils <nilsammersken@gmail.com>
2021-01-26 10:18:00 +0100

    Add Bookmarks and Create Questions Branch

---
+++ components/create-question.stories.js
@@ -0,0 +1,10 @@
+import '../styles/index.css'
+
+import './create-question.css'
+import createquestion from './create-question.html'
+
+export default {
+  title: 'Create-question',
+}
+
+export const base = () => createquestion
