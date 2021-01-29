e32d0de44baa00bb2941efd1f0179b45ddfc4c26
Ammersken Nils <nilsammersken@gmail.com>
2021-01-29 01:01:38 +0100

    Add create-question page and adjust navbar

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,8 @@
-function getElement(dataJsName) {
-  return document.querySelector(`[data-js="${dataJsName}"]`)
-}
-
 const heading = getElement('universal-header')
-
 const pageHome = getElement('main-home')
 const pageBookmark = getElement('main-bookmark')
+const pageCreate = getElement('main-create')
+
 const navButton1 = getElement('navHome')
 const navButton2 = getElement('navBookmark')
 const navButton3 = getElement('navCreate')
@@ -14,15 +11,33 @@ const navButton4 = getElement('navSettings')
 navButton1.addEventListener('click', () => {
   pageHome.classList.remove('hidden')
   pageBookmark.classList.add('hidden')
+  pageCreate.classList.add('hidden')
   navButton1.classList.add('navbar__item--active')
   navButton2.classList.remove('navbar__item--active')
+  navButton3.classList.remove('navbar__item--active')
   heading.textContent = 'QUIZ-APP'
 })
 
 navButton2.addEventListener('click', () => {
   pageHome.classList.add('hidden')
   pageBookmark.classList.remove('hidden')
+  pageCreate.classList.add('hidden')
   navButton1.classList.remove('navbar__item--active')
   navButton2.classList.add('navbar__item--active')
+  navButton3.classList.remove('navbar__item--active')
   heading.textContent = 'BOOKMARKS'
 })
+
+navButton3.addEventListener('click', () => {
+  pageHome.classList.add('hidden')
+  pageBookmark.classList.add('hidden')
+  pageCreate.classList.remove('hidden')
+  navButton1.classList.remove('navbar__item--active')
+  navButton2.classList.remove('navbar__item--active')
+  navButton3.classList.add('navbar__item--active')
+  heading.textContent = 'CREATE QUESTION'
+})
+
+function getElement(dataJsName) {
+  return document.querySelector(`[data-js="${dataJsName}"]`)
+}
